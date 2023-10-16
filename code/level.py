c42b354bedf019d05208351256ec28c681f793dc
GabrielPirovane <gabriel.balique@gmail.com>
2023-10-15 21:41:23 -0300

    Adicionado: câmera, overlap e colisão

--- code/level.py
+++ code/level.py
@@ -1,4 +1,8 @@
-import pygame 
+import pygame
+from settings import *
+from tile import Tile
+from player import Player
+
 
 class Level:
     def __init__(self):
@@ -7,9 +11,43 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
 		#sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
+
+        # sprite set up
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+
 
     def run(self):
         #Update and draw the game
-        pass
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        #general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+    
+    def custom_draw(self, player):
+
+        # gettint the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        for sprite in sorted(self.sprites(), key= lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
--- code/tile.py
+++ code/tile.py
@@ -1,10 +1,12 @@
 import pygame
-from pygame.sprite import _Group 
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(toplef = pos)
+        self.image = pygame.image.load('./graphics/test/rock.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
+
+        # Retira 5 pixels de cada borda da hitbox original
+        self.hitbox = self.rect.inflate(0, -10)
         
